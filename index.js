26d9d6c6d54bf18150776ebf5dfe4d0d9d2f8dc8
Daniel Boley <danboley1@gmail.com>
2022-08-31 08:39:50 -0400

    commit tree 843a2a46d2a905e8014c5151e9a3e0bc33fa384f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(
+        function(driver) {
+            return driver.toLowerCase() === string.toLowerCase()
+        }
+    )
+}
+
+ //   for (const user of collection) {
+ //       if (drivers.name === drivers.name) {
+ //           console.log(drivers.name)
+  //      }
+ //   }
+//}
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(
+        function(driver) {
+            return driver.toLowerCase().substring(0, string.length) === string.toLowerCase()
+        }
+    )
+};
+
+//function(driver) {
+//    return driver.firstInitial[0] === string.firstInitial[0]
+//}
+
+
+function matchName(drivers, hometown) {
+    return drivers.filter(
+        function(driver) {
+            return driver.name === hometown
+        }
+    )
+};
